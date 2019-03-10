e289c0f9a8497a7ed2943ffe1033ce6146e683c1
sunitparekh <parekh.sunit@gmail.com>
2019-03-10 22:44:13 +0530

    #2 TODO added

--- src/main/kotlin/com/github/seed/FileResourceReader.kt
+++ src/main/kotlin/com/github/seed/FileResourceReader.kt
@@ -6,6 +6,7 @@ import java.io.BufferedReader
 class FileResourceReader {
 
     fun readFile(fileName: String): BufferedReader {
+        // TODO change it to use java.nio.* for reading as non-blocking way
         val stream = readFileFromClassPath(fileName) ?: readFileFromJarFileClassPath(fileName)
         return BufferedReader(stream.reader())
     }
